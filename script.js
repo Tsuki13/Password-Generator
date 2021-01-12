3a775302af98211f1e5339ac5a801f7b0c57f22d
Rachel Diaz <Rachel7113@gmail.com>
2021-01-11 22:43:52 -0500

    Clarified var names and fixed password var location

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
  passwordText.value = password;
- var password = '';
+ 
 }
-
-
-
 //function that prompts user for criteria
 function generatePassword(){
   var length1 = parseInt(prompt("Chose password length between 8-128 characters"));
@@ -19,50 +16,50 @@ function generatePassword(){
     alert("length needs to be between 8 & 128 characters!");
     return;
   }
-  //array for confirmed criteria 
-  var inputVal = []; 
+  var characterOptions = [];  //array for confirmed criteria 
 
+  var password1 = '';
   var upperletters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var lowerletters = 'abcdefghijklmnopqrstuvwxyz';
   var characters = '!@#$%^&*_-+?~,`';
   var numbers = '1234567890';
 
-  //Add var to inputVal array if user confirms
+  //Add vars to characterOption if user confirms
   var lowercase = confirm("Do you want to use lowercase letters?");
   console.log(lowercase);
   if (lowercase == true){
-    inputVal.push(lowerletters);
+    characterOptions.push(lowerletters);
   }
   var uppercase = confirm("Do you want to use uppercase letters?");
   console.log(uppercase);
   if (uppercase == true){
-    inputVal.push(upperletters);
+    characterOptions.push(upperletters);
   }
   var numeric = confirm("Do you want to use numbers?");
   console.log(numeric);
   if (numeric == true){
-    inputVal.push(numbers);
+    characterOptions.push(numbers);
   }
   var specialchar = confirm("Do you want to use special characters?");
   console.log(specialchar);
   if (specialchar == true){
-    inputVal.push(characters);
+    characterOptions.push(characters);
   } 
-  console.log(inputVal)
+  console.log(characterOptions)
 
-//loop to pull random characters from inputVal array
+//loop to pull random characters from characterOptions
   for (var i = 1; i <= length1; i++) {
-    var inputChoices = (Math.floor(Math.random() *inputVal.length));
-    var randomSelectionarr = inputVal[inputChoices];
-    var lengthbyuser = Math.floor(Math.random() *randomSelectionarr.length);
-    var randomPick = randomSelectionarr[lengthbyuser];
-    password += randomPick;
- 
+    var randomNum = (Math.floor(Math.random()*characterOptions.length));
+    var randomSelection = characterOptions[randomNum];
+
+    var randomCharNum = Math.floor(Math.random()*randomSelection.length);
+    var randomPick = randomSelection[randomCharNum];
+    //add the random character to password
+    password1 += randomPick;
     console.log(randomPick)
+    
   }
-
-
-  return password;
+  return password1;
 }
 
 // Add event listener to generate button
